ba477e7dc6188e1d343517235a6bfb7b76cfe759
Bogdan Fedorenko <bogdan.fedorenko777@gmail.com>
2018-10-31 12:32:30 +0300

    Tested xmlSerializer with array of objects

--- Term3/Lab3/ConsoleProjectForTests/Program.cs
+++ Term3/Lab3/ConsoleProjectForTests/Program.cs
@@ -16,17 +16,27 @@ namespace ConsoleProjectForTests
         static void Main(string[] args)
         {
             Product product = new Product("name", "1", new DateTime(2018, 10, 07), 90);
+            Product product1 = new Product("name", "1", new DateTime(2018, 10, 07), 90);
+            Product product2 = new Product("name", "1", new DateTime(2018, 10, 07), 90);
+            Product[] products = new Product[] { product, product1, product2 };
             string filePath = @"C:\Progbase\Study\Term3\Lab3\ConsoleProjectForTests\Products.xml";
 
-            XmlDataSerializer<Product> xmlProductSerializer = new XmlDataSerializer<Product>();
-            xmlProductSerializer.Serialize(product, filePath, FileMode.Truncate);
 
-            Product newProduct = xmlProductSerializer.Deserialize(filePath, FileMode.Open);
 
-            Console.WriteLine("Name " + newProduct.Name);
-            Console.WriteLine("Id " + newProduct.Id);
-            Console.WriteLine("DateTime " + newProduct.DateOfManufacture.ToString());
-            Console.WriteLine("Lifetime " + newProduct.Lifetime);
+
+            XmlDataSerializer<Product[]> xmlProductSerializer = new XmlDataSerializer<Product[]>();
+            xmlProductSerializer.Serialize(products, filePath, FileMode.Truncate);
+
+
+
+            Product[] newProducts = xmlProductSerializer.Deserialize(filePath, FileMode.Open);
+            foreach (Product pr in newProducts)
+            {
+                Console.WriteLine("Name " + pr.Name);
+                Console.WriteLine("Id " + pr.Id);
+                Console.WriteLine("DateTime " + pr.DateOfManufacture.ToString());
+                Console.WriteLine("Lifetime " + pr.Lifetime);
+            }
         }
     }
 }

e400bf52be35af4b17ee7632ec9b6b890f61e502
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-02 20:38:25 +0300

    Added json service.

---
+++ Term3/Lab3/BusinessLogicLayer.Services/Services/JsonSerializerService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicLayer.Services.Intefaces;
+using DataAccessLayer.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.Services
+{
+    public class JsonSerializerService<T> : SerializationService<T> where T : class
+    {
+        public JsonSerializerService()
+        {
+            Serializer = new JsonSerializer<T>();
+        }
+    }
+}

76ac6fff7c10720771baa55969f64bf0831af4c5
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-05 21:53:58 +0300

    Added temporary validation

--- Term3/Lab3/BusinessLogicLayer.Entities/Product.cs
+++ Term3/Lab3/BusinessLogicLayer.Entities/Product.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 
 namespace BusinessLogicLayer.Entities
 {
@@ -18,13 +19,43 @@ namespace BusinessLogicLayer.Entities
         private DateTime expirationDate;
 
         [DataMember]
-        public string Name { get => name; set => name = value; }
+        public string Name { get => name;
+            set
+            {
+                if (!Regex.IsMatch(value, @"\w+|\s+"))
+                {
+                    name = "Invalid characters!";
+                    return;
+                }
+                name = value;
+            }
+        }
         [DataMember]
-        public string Id { get => id; set => id = value; }
+        public string Id { get => id;
+            set
+            {
+                if (!Regex.IsMatch(value, @"\w\w\d{4}"))
+                {
+                    id = "Invalid characters!";
+                    return;
+                }
+                id = value;
+            }
+        }
         [DataMember]
         public DateTime DateOfManufacture { get => dateOfManufacture; set => dateOfManufacture = value; }
         [DataMember]
-        public int Lifetime { get => lifetime; set => lifetime = value; }
+        public int Lifetime { get => lifetime;
+            set
+            {
+                if (value < 0 || value > 300)
+                {
+                    lifetime = 0;
+                    return;
+                }
+                lifetime = value;
+            }
+        }
         [DataMember]
         public DateTime ExpirationDate { get => expirationDate; set => expirationDate = value; }
 
--- Term3/Lab3/BusinessLogicLayer.Entities/Student.cs
+++ Term3/Lab3/BusinessLogicLayer.Entities/Student.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.Entities
@@ -14,9 +15,49 @@ namespace BusinessLogicLayer.Entities
         private int birthYear;
         private string groupId;
 
-        public string Id { get => id; set => id = value; }
-        public string FirstName { get => firstName; set => firstName = value; }
-        public string LastName { get => lastName; set => lastName = value; }
-        public int BirtYear { get => birthYear; set => birthYear = value; }
+        public string Id { get => id;
+            set
+            {
+                if (!Regex.IsMatch(value, @"\w\w\d{4}"))
+                {
+                    id = "Invalid characters!";
+                    return;
+                }
+                id = value;
+            }
+        }
+        public string FirstName { get => firstName;
+            set
+            {
+                if (!Regex.IsMatch(value, @"\w+|\s+"))
+                {
+                    firstName = "Invalid characters!";
+                    return;
+                }
+                firstName = value;
+            }
+        }
+        public string LastName { get => lastName;
+            set
+            {
+                if (!Regex.IsMatch(value, @"\w+|\s+"))
+                {
+                    lastName = "Invalid characters!";
+                    return;
+                }
+                lastName = value;
+            }
+        }
+        public int BirtYear { get => birthYear;
+            set
+            {
+                if (value < 1900 || value > DateTime.Now.Year)
+                {
+                    birthYear = -1;
+                    return;
+                }
+                birthYear = value;
+            }
+        }
     }
 }

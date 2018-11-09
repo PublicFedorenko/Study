b4da98fd1650d762599d1f6af52a35a0b39c0b98
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-09 12:49:15 +0300

    Initial commit

---
+++ Term3/Lab4/Events/CustomList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class CustomList<T>
+    {
+        private List<T> _list;
+
+        public void Add(T item)
+        {
+
+        }
+
+        public void Insert(int index, T item)
+        {
+
+        }
+
+        public void Remove(T item)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
---
+++ Term3/Lab4/Events/ListEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class ListEventArgs : EventArgs
+    {
+        public string Message { get; }
+        public int Length { get; }
+
+        public ListEventArgs(string mess, int len)
+        {
+
+        }
+    }
+}

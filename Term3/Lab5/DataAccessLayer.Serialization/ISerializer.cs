10ba600f16575d4a272378c807b8ca91eb4381ba
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-10 12:47:09 +0300

    implemented xml serialization

---
+++ Term3/Lab5/DataAccessLayer.Serialization/ISerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataAccessLayer.Serialization
+{
+    interface ISerializer<T> where T : class
+    {
+        void Serialize(T item, string filePath, FileMode fileMode);
+        T Deserialize(string filePath, FileMode fileMode);
+    }
+}
--- Term3/Lab5/DataAccessLayer.Serialization/XmlSerializer.cs
+++ Term3/Lab5/DataAccessLayer.Serialization/XmlSerializer.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace DataAccessLayer.Serialization
 {
-    public class XmlSerializer
+    public class XmlSerializer<T> : ISerializer<T> where T : class
     {
+        private XmlSerializer xmlSerializer;
 
+        public XmlSerializer()
+        {
+            xmlSerializer = new XmlSerializer(typeof(T));
+        }
+        public void Serialize(T item, string filePath, FileMode fileMode)
+        {
+            using (FileStream fs = new FileStream(filePath, fileMode))
+            {
+                xmlSerializer.Serialize(fs, item);
+            }
+        }
+
+        public T Deserialize(string filePath, FileMode fileMode) 
+        {
+            using (FileStream fs = new FileStream(filePath, fileMode))
+            {
+                return (T)xmlSerializer.Deserialize(fs);
+            }
+        }
     }
 }

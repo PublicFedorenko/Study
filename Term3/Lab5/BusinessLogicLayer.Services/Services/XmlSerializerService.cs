0148c459f2e870162341020af7692233b67cb152
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2018-11-10 14:40:55 +0300

    added xml serializer service

---
+++ Term3/Lab5/BusinessLogicLayer.Services/Services/XmlSerializerService.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLogicLayer.Services.Interfaces;
+using DataAccessLayer.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BusinessLogicLayer.Services.Services
+{
+    public class XmlSerializerService<T> : ISerializerService<T> where T : class
+    {
+        private XmlSerializer<T> xmlSerializer;
+
+        public XmlSerializerService()
+        {
+            xmlSerializer = new XmlSerializer<T>();
+        }
+
+        public void Serialize(T item, string filePath, FileMode fileMode)
+        {
+            xmlSerializer.Serialize(item, filePath, fileMode);
+        }
+
+        public T Deserialize(string filePath, FileMode fileMode)
+        {
+            return xmlSerializer.Deserialize(filePath, fileMode);
+        } 
+    }
+}
